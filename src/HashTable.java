7c55bc31e26e43f8e92898ea4d0534fdc10b613e
StrayBird_ATSH <zhanghuilian-98022@163.com>
2018-10-10 16:14:54 +0800

    Draft version for the hashInsert method

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,44 @@
 public class HashTable {
+    private Student[] students = new Student[100];
+
+    void hashSearch() {
+
+    }
+
+    Student hashInsert(String key, Student studentObject) {
+        // Make sure the studentObject is not null
+        if (studentObject == null) {
+            throw new NullPointerException();
+        }
+
+        // Makes sure the key is not already in the hashtable.
+        int probe = 0;
+        int index = hashFunction(key, probe);
+        for (; students[index] != null; index = hashFunction(key, probe)) {
+            Student oldStudent = students[index];
+            if (oldStudent.getName().equals(key)) {
+                students[index] = studentObject;
+                return oldStudent;
+            }
+            probe++;
+        }
+
+        probe = 0;
+        index = hashFunction(key, probe);
+        for (; ; probe++, index = hashFunction(key, probe)) {
+            if (students[index] != null) continue;
+            students[index] = studentObject;
+            break;
+        }
+        return null;
+    }
+
+    void hashDelete() {
+    }
+
+    private int hashFunction(String key, int probe) {
+        key = key.concat(Integer.toString(probe));
+        int hash = key.hashCode();
+        return (hash & 0x7FFFFFFF) % 100;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,45 @@
 public class Student {
+    private int studentID;
+    private String name;
+    private String major;
+    private String gender;
+
+    public Student(int studentID, String name, String major, String gender) {
+        this.studentID = studentID;
+        this.name = name;
+        this.major = major;
+        this.gender = gender;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
